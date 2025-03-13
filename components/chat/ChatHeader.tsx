e02b5790daef63e9c9fee3340411dd7bec303487
Matthieu Mordrel <matthieumordrel@gmail.com>
2025-03-13 11:44:59 +0100

    minor update

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -16,16 +16,7 @@ const ChatHeader = ({ title = "New Conversation" }: { title?: string }) => {
   return (
     <div className="mb-4 flex items-center justify-between border-b pb-2">
       <h1 className="text-xl font-bold">{titleClient}</h1>
-      <div className="flex gap-2">
-        {/* TO DO: Handle creating new conversations */}
-        {/* <Button
-          variant="ghost"
-          onClick={clearConversation}
-          title="Start a new conversation"
-        >
-          Clear Chat
-        </Button> */}
-      </div>
+      <div className="flex gap-2"></div>
     </div>
   );
 };
