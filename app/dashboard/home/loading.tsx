9402f37a7c3abcefc44524a82cc3b27bdb37476d
Matthieu Mordrel <matthieumordrel@gmail.com>
2025-03-18 11:47:40 +0100

    new update

--- app/dashboard/home/loading.tsx
+++ app/dashboard/home/loading.tsx
@@ -1,10 +1,5 @@
-import { ConversationSidebarSkeleton } from "@/components/skeletons/ConversationSidebarSkeleton";
 import SkeletonChat from "@/components/skeletons/SkeletonChat";
+
 export default function DashboardLoading() {
-  return (
-    <>
-      <ConversationSidebarSkeleton />
-      <SkeletonChat />
-    </>
-  );
+  return <SkeletonChat />;
 }
