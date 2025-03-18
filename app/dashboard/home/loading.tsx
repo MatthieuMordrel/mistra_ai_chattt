9a0dd52e1105d5bb466072a4d504d01f7450cd69
Matthieu Mordrel <matthieumordrel@gmail.com>
2025-03-18 11:45:31 +0100

    removed dashboard layout in loading js

--- app/dashboard/home/loading.tsx
+++ app/dashboard/home/loading.tsx
@@ -1,11 +1,10 @@
-import { DashboardLayoutClient } from "@/components/dashboard/DashboardLayoutClient";
 import { ConversationSidebarSkeleton } from "@/components/skeletons/ConversationSidebarSkeleton";
 import SkeletonChat from "@/components/skeletons/SkeletonChat";
 export default function DashboardLoading() {
   return (
-    <DashboardLayoutClient>
+    <>
       <ConversationSidebarSkeleton />
       <SkeletonChat />
-    </DashboardLayoutClient>
+    </>
   );
 }
