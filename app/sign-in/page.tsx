3a90d35c303572968c9b7182d14a6530392aefe7
MatthieuMordrel <matthieumordrel@gmail.com>
2025-03-25 18:43:28 +0100

    whitelisting message on signin

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -5,7 +5,15 @@ export default function SignInPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <NavBar showHome={true} />
-      <div className="flex flex-1 items-center justify-center">
+      <div className="flex flex-1 flex-col items-center justify-center gap-4">
+        <div className="mb-2 text-center">
+          <p className="text-lg font-medium">
+            You must be whitelisted to access the website!
+          </p>
+          <p className="text-muted-foreground text-sm">
+            Please send a mail to matthieumordrel@gmail.com if needed
+          </p>
+        </div>
         <SignIn />
       </div>
     </div>
